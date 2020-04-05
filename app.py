c653a4933d91a41ba0177ff6a4cbd741a28f51ab
Jasper Lorenz <jasper@lorenzmail.de>
2020-04-05 14:02:03 +0200

    Made the sauce shorter

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pynput.mouse as p
-import time as t
+import time
 m,x,y=p.Controller(),1,1
 while 1:
     m.move(x,y)
-    t.sleep(1e-9)
+    time.sleep(1e-9)
     x=-x if m.position[0] in[1918,0] else x
     y=-y if m.position[1] in[1198,0] else y
