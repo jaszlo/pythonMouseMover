be3c0834abfe739f7afd7887bed786f1d8d90b13
jasper@lorenzmail.de <jasper@lorenzmail.de>
2020-04-05 16:20:19 +0200

    Changed 'if' checking for border to shorter way

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ m,x,y=p.Controller(),1,1
 while 1:
     m.move(x,y)
     time.sleep(1e-9)
-    x=-x if m.position[0] in[1918,0] else x
-    y=-y if m.position[1] in[1078,0] else y
+    x*=1-2*(m.position[0] in[1918,0])    
+    y*=1-2*(m.position[1] in[1078,0])
