b20cb3e708c8a8947bc29344c0892ebbe864146f
Jasper Lorenz <55958177+jasZnerol@users.noreply.github.com>
2020-04-05 18:27:48 +0200

    Update HolzmindenScherfede1.0.py

--- HolzmindenScherfede1.0.py
+++ HolzmindenScherfede1.0.py
@@ -1,3 +1,3 @@
 import pynput.mouse as m,time
-c,d,r=m.Controller(),[1,1],[1918,1198]
+c,d,r=m.Controller(),[1,1],[1918,1078]
 while 1:c.move(*d);time.sleep(1e-9);d=[-d[i]if c.position[i]in[0,r[i]]else d[i]for i in[0,1]]
