3b108eeaa7de0b057264f07f0d97e2b01bde708d
Jasper L <55958177+jaszlo@users.noreply.github.com>
2021-04-01 02:16:19 +0200

    Update app.py
    Variation of HolzmindenSchmiedes Solution with viewest lines of code

--- app.py
+++ app.py
@@ -1,7 +1,2 @@
-import pynput.mouse as p, time
-m,x,y=p.Controller(),1,1
-while 1:
-    m.move(x,y)
-    time.sleep(1e-9)
-    x*=1-2*(m.position[0] in[1918,0])    
-    y*=1-2*(m.position[1] in[1078,0])
+import pynput.mouse as m,time;c,d,r=m.Controller(),(1,1),(1918,1078)
+while 1:c.move(*d);time.sleep(1e-9);d=[-d[i]if c.position[i]in[0,r[i]]else d[i]for i in[0,1]]
